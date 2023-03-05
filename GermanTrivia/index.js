edab6991e839b61f3f229e21bcdbb0526ae37868
Berdi <Benedikt.Daniel1@web.de>
2023-03-05 11:53:25 +0100

    Install Firebase

---
+++ GermanTrivia/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAvzYSN4-7FMUpOhzdsaV4psBjqQWF75Rw",
+  authDomain: "german-trivia-6738d.firebaseapp.com",
+  projectId: "german-trivia-6738d",
+  storageBucket: "german-trivia-6738d.appspot.com",
+  messagingSenderId: "349055401678",
+  appId: "1:349055401678:web:4f8a377bcc8d1baaddda9f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
